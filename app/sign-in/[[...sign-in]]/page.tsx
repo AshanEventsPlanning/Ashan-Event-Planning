8b2768083889a35b069476bd2f85218f582945f5
keshan <waadissanayake@gmail.com>
2024-04-22 13:31:56 +0530

    after signin signup url

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignIn path="/sign-in?redirect_url=https://space-rouge-five.vercel.app
-    " />;
+    return <SignIn afterSignInUrl={'/'} />;
 }
