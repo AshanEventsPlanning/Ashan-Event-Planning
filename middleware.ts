4e9976e3c18bccbddd436155b0350f8422b374ac
keshan <waadissanayake@gmail.com>
2024-04-22 15:34:32 +0530

    middleware test

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-    '/(.*)',
-  ]);
+// const isProtectedRoute = createRouteMatcher([
+//     '/(.*)',
+//   ]);
  
-  export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
-  });
+//   export default clerkMiddleware((auth, req) => {
+//     if (isProtectedRoute(req)) auth().protect();
+//   });
+ 
+// export const config = {
+//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+import { clerkMiddleware } from "@clerk/nextjs/server";
+ 
+export default clerkMiddleware();
  
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.\\..|_next).)', '/', '/(api|trpc)(.)'],
 };
