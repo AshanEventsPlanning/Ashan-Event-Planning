21518423c2ffd3327a8afef1b7d42262fc0c03d0
keshan <waadissanayake@gmail.com>
2024-04-22 13:20:36 +0530

    redirect

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <div className="flex justify-center items-center"><SignUp path="/sign-up" /></div>;
+  return <div className="flex justify-center items-center"><SignUp path="/sign-up?redirect_url=https://space-rouge-five.vercel.app" /></div>;
 }
