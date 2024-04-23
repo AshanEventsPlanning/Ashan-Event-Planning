fbe9f237d1e712de46f8c00f7bb753a5740829f9
keshan <waadissanayake@gmail.com>
2024-04-23 12:59:19 +0530

    error fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,14 @@ import InfoForm from '@/components/info-form'
 import Report from '@/components/report'
 import { UserButton } from "@clerk/nextjs"
 import { currentUser } from '@clerk/nextjs/server';
+import { auth } from '@clerk/nextjs/server';
 
 export default async function Home() {
-  const user = await currentUser();
-  const userId = (user)? user.id : ""
-
+  let { userId } : { userId: string | null } = auth();
+  if(userId == null){
+    userId= ""
+  }
+  console.log(userId)
   return (
     <div>
       <InfoForm  userId ={userId}/>
