13337ab89253e94f78c3d993b80a195de36af446
keshan <waadissanayake@gmail.com>
2024-05-12 09:19:26 +0530

    Image added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import InfoForm from '@/components/info-form'
-import background from "../public/background.webp";
+import background from "../public/background.jpeg";
+import Image from 'next/image'
 
-export default  function Home() {
+export default function Home() {
 
   return (
-    <div>
+    <div className='background'>
       <InfoForm />
     </div>
   )
